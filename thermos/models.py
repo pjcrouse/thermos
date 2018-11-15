d11e9535981eef13bba0d006c9b80530f3474787
pcrouse <patcrouse@gmail.com>
2018-11-15 15:53:28 -0500

    complete with unit testing.

--- thermos/models.py
+++ thermos/models.py
@@ -30,6 +30,8 @@ class Bookmark(db.Model):
     def tags(self, string):
         if string:
             self._tags = [Tag.get_or_create(name) for name in string.split(',')]
+        else:
+            self._tags = []
 
     def __repr__(self):
         return f"<Bookmark '{self.description}': '{self.url}'>"
